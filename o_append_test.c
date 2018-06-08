d60eb830a3f0b2729835e5c1aaef2428ad5a62e7
ydong08 <lfeng1108@163.com>
2018-06-08 16:17:43 +0800

    update code

---
+++ o_append_test.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+
+	int a = 1<<12, b = 1<<12;
+	int i = 0;
+
+	char *a_array = malloc(a);
+	char *b_array = malloc(b);
+
+	memset(a_array, 'a', a);
+	memset(b_array, 'b', b);
+
+	int fd = open("ab_o_append.txt", O_WRONLY|O_APPEND);
+
+	if (fd == -1) {
+		perror("open()");
+		return -1;
+	}
+
+	switch (fork()) {
+	
+		case -1:
+			perror("fork() error");
+			return -1;
+		case 0:
+			printf("parent\n");
+			for ( i=0; i < a; i += 64){
+				if (-1 == write(fd, a_array+i, 64)) {
+					perror("parent write");
+				}
+			}
+			break;
+		default:
+			printf("child\n");
+			for ( i=0; i<a; i += 64) {
+				if (-1 == write(fd, b_array+i, 64)) {
+					perror("child write");
+				}
+			}
+			break;
+	}
+
+	free(a_array);
+	free(b_array);
+
+	close(fd);
+
+	return 0;
+}
---
+++ strstr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    const char* pSrc = "abcdefghijk";
+    const char* pSub = "hji";
+    char *p = strstr(pSrc, pSub);
+    if (NULL == p)
+    {
+        printf("not find\n");
+    }
+    else
+    {
+        int index = p - pSrc;
+        printf("%s\n", p);
+        printf("index: %d\n", index);
+        printf("%s\n", pSrc + index);
+    }
+
+    return 0;
+}
+
