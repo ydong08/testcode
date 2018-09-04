aef1c5bb901d187a033f18f41828b950c1816037
ydong08 <lfeng1108@163.com>
2018-09-04 16:12:29 +0800

    add sigaction test code

--- signal/sigint.cpp
+++ signal/sigint.cpp
@@ -1,44 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
 
-//#define POSIX_SIGINFO
-
-void myhandler(int signo) {
-    if (SIGINT == signo) {
-        printf("recv SIGINT signal\n");
-        printf("++++++++++++++++\n");
-    }
-}
-
+#define SIG_BUF_LEN        256 
 void myaction(int signo, siginfo_t* psig, void* pdata) {
     if (SIGINT == signo) {
         printf("recv SIGINT signal\n");
         printf("sigval: %d\n", psig->si_value.sival_int);
-        printf("****************\n");
+        printf("*****************\n");
     }
 }
 
 int main() {
+    char buf[SIG_BUF_LEN] = {0};
     struct sigaction sign;
     struct sigaction sigo;
     memset(&sign, 0x00, sizeof(sign));
     memset(&sigo, 0x00, sizeof(sigo));
-    // SA_RESTART 会让中断的调用在信号处理完成后自动执行
-#ifdef POSIX_SIGINFO
     sign.sa_flags = SA_SIGINFO|SA_RESTART;
-    sign.sa_sigaction = myaction;
-#else
-    // sign.sa_flags = 0;
-    sign.sa_flags = SA_SIGINFO|SA_RESTART;
-    sign.sa_handler = myhandler;
-#endif
     sigemptyset(&sign.sa_mask);
-    sigaddset(&sign.sa_mask, SIGINT);
+    sign.sa_sigaction = myaction;
     int retval = sigaction(SIGINT, &sign, &sigo);
     if (retval < 0) {
         printf("sigaction: %d\n", errno);
@@ -46,15 +31,23 @@ int main() {
     }
     union sigval sigv;
     memset(&sigv, 0x00, sizeof(sigv));
-    sigv.sival_int = 1000;
+    sigv.sival_int = 1234;
     retval = sigqueue(getpid(), SIGINT, sigv);
     if (retval < 0) {
-        printf("sigqueue: %d\n", errno);
+        printf("sigqueue: %d\n", retval);
         exit(1);
     }
+
+    memset(buf, 0x00, sizeof(buf));
+    retval = read(0, buf, 100);
+    if (retval < 0) {
+        printf("read fail: %s\n", strerror(errno));
+        perror("read");
+        exit(1);
+    }
+    buf[sizeof(buf)-1] = '\0';
+    printf("read %d bytes\n", retval);
+    printf("read buf: %s\n", buf);
     return 0;
 }
-
-
-
 
