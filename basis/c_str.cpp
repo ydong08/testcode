72bb446bdd9c7e7e689977927d28e6e14840c314
ydong08 <lfeng1108@163.com>
2018-11-19 11:43:38 +0800

    upload code

---
+++ basis/c_str.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+void print(const char* pstr) {
+    printf("str: %s, len: %d\n", pstr, strlen(pstr));
+}
+int main() {
+    string str = string("0987654321qwertyyuiioopplkjhgfdsazxcvbnm");
+    string str1 = string("# 10:05:27 0x42E69490 CommTcp.cpp-00920:getsockopt nRet:0, status:7, info:TCP_CLOSE Host:[192.168.31.124:5020]");
+
+    const char* p = str.c_str();
+    const char* p1 = str1.c_str();
+
+    print(p);
+    print(p1);
+
+    return 0;
+
+}
