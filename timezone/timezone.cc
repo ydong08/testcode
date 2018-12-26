9c1a3576bebef2f45614c4b5d2472074db46b82c
ydong08 <lfeng1108@163.com>
2018-12-26 17:39:54 +0800

    add

--- timezone/timezone.cc
+++ timezone/timezone.cc
@@ -9,6 +9,7 @@ int main() {
     memset(&tms, 0x00, sizeof(tms));
     time_t tt = time(NULL);
     localtime_r(&tt, &tms);
+    printf("DST: %d\n", tms.tm_isdst);
     printf("Date: %04d-%02d-%02d\n", tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday);
     printf("Time: %02d-%02d-%02d\n", tms.tm_hour, tms.tm_min, tms.tm_sec);
     return 0;
