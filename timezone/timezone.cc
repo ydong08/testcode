7fc3c79b6bcaab7fe35f342a0e624a61348a0a42
ydong08 <lfeng1108@163.com>
2018-12-26 17:25:52 +0800

    add timezone

--- timezone/timezone.cc
+++ timezone/timezone.cc
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    struct tm tms;
+    memset(&tms, 0x00, sizeof(tms));
+    time_t tt = time(NULL);
+    localtime_r(&tt, &tms);
+    printf("Date: %04d-%02d-%02d\n", tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday);
+    printf("Time: %02d-%02d-%02d\n", tms.tm_hour, tms.tm_min, tms.tm_sec);
+    return 0;
+
+}
