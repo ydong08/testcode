94a2aba8094cc3f0a149383c301ca53c09b00cba
ydong08 <lfeng1108@163.com>
2018-12-27 11:25:24 +0800

    add time api

--- timezone/timezone.cc
+++ timezone/timezone.cc
@@ -4,15 +4,49 @@
 #include <unistd.h>
 #include <string.h>
 
+
 int main() {
     struct tm tms;
     memset(&tms, 0x00, sizeof(tms));
+
+    /*######### time_t -> time string ###########*/
     time_t tt = time(NULL);
-    localtime_r(&tt, &tms);
+    printf("time: %ld\n", tt);
+    // 1. time_t -> tm
+    // gmtime(&tt, &tms);  // no timezone
+    localtime_r(&tt, &tms); // + timezone
     printf("UTC: %ld\n", (long)tt);
     printf("DST: %d\n", tms.tm_isdst);
     printf("Date: %04d-%02d-%02d\n", tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday);
     printf("Time: %02d-%02d-%02d\n", tms.tm_hour, tms.tm_min, tms.tm_sec);
+
+    // 1. asctime: tm -> fix time string
+    memset(tbuf, 0x00, sizeof(tbuf));
+    printf("asctime: %s\n", asctime_r(&tms, tbuf));
+
+    // 2. strftime: tm -> fix time string
+    memset(tbuf, 0x00, sizeof(tbuf));
+    printf("strftime: %s\n", strftime(tbuf, 
+                                      sizeof(tbuf), 
+                                      "%04d-%02d-%02d %02d:%02d:%02d", 
+                                      tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday,
+                                      tms.tm_hour, tms.tm_min, tms.tm_sec));
+
+    // time_t -> fix time string
+    const int TIME_STRING_LEN = 20;
+    char tbuf[TIME_STRING_LEN] = {0};
+    printf("ctime: %s\n", ctime_r(&tt, tbuf));
+
+    /*######### time string -> time_t ###########*/
+    // strptime: fix time string -> tm 
+    strptime(tbuf, "%c", &tms);
+    printf("Date: %04d-%02d-%02d\n", tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday);
+    printf("Time: %02d-%02d-%02d\n", tms.tm_hour, tms.tm_min, tms.tm_sec);
+
+    // mktime: tm -> time_t
+    tt = mktime(&tms);
+    printf("time: %ld\n", tt);
+
     return 0;
 
 }
