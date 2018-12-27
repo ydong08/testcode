560864d06551ae199e0680ad627137270c495560
ydong08 <lfeng1108@163.com>
2018-12-27 11:55:59 +0800

    modification

--- timezone/timezone.cc
+++ timezone/timezone.cc
@@ -8,6 +8,8 @@
 int main() {
     struct tm tms;
     memset(&tms, 0x00, sizeof(tms));
+    const int TIME_STRING_LEN = 32;
+    char tbuf[TIME_STRING_LEN] = {0};
 
     /*######### time_t -> time string ###########*/
     time_t tt = time(NULL);
@@ -26,26 +28,21 @@ int main() {
 
     // 2. strftime: tm -> fix time string
     memset(tbuf, 0x00, sizeof(tbuf));
-    printf("strftime: %s\n", strftime(tbuf, 
-                                      sizeof(tbuf), 
-                                      "%04d-%02d-%02d %02d:%02d:%02d", 
-                                      tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday,
-                                      tms.tm_hour, tms.tm_min, tms.tm_sec));
+    strftime(tbuf, sizeof(tbuf), "%c", &tms);
+    printf("strftime: %s\n", tbuf);
 
     // time_t -> fix time string
-    const int TIME_STRING_LEN = 20;
-    char tbuf[TIME_STRING_LEN] = {0};
     printf("ctime: %s\n", ctime_r(&tt, tbuf));
 
     /*######### time string -> time_t ###########*/
     // strptime: fix time string -> tm 
     strptime(tbuf, "%c", &tms);
-    printf("Date: %04d-%02d-%02d\n", tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday);
-    printf("Time: %02d-%02d-%02d\n", tms.tm_hour, tms.tm_min, tms.tm_sec);
+    printf("[strptime]Date: %04d-%02d-%02d\n", tms.tm_year+1900, tms.tm_mon+1, tms.tm_mday);
+    printf("[strptime]Time: %02d-%02d-%02d\n", tms.tm_hour, tms.tm_min, tms.tm_sec);
 
     // mktime: tm -> time_t
     tt = mktime(&tms);
-    printf("time: %ld\n", tt);
+    printf("[mktime]time: %ld\n", tt);
 
     return 0;
 
