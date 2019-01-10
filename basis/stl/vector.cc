e0d4a0dddd3fef62699060511ed38f08a85caccf
ydong08 <lfeng1108@163.com>
2019-01-10 09:42:40 +0800

    add stl/vector

---
+++ basis/stl/vector.cc
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
+const int NAME = 5;
+int main() {
+  vector<string> vecName;
+  const char* pName = "emv_config.xml";
+  const char* arrName[NAME] = {"castlspay.xml", 
+                               "castlespaycon.xml", 
+                               "castlespaycloud.xml",
+                               "emvcl_config.xml",
+                               "emv_config.xml"};
+//  vecName.push_back(pName);
+  for (int i = 0; i < NAME; ++i) {
+    vecName.push_back(arrName[i]);
+  }
+
+  for (int i = 0; i < NAME; ++i) {
+    if (!memcmp(vecName[i].c_str(), pName, strlen(pName)))
+      cout << vecName[i] << endl;
+  }
+
+  return 0;
+
+}
--- basis/string/string.cc
+++ basis/string/string.cc
@@ -1,5 +1,6 @@
-/*统计给定字符串中的最长子字符串:
-   子字符串由同一字符构成*/
+
+
+	
 int  ChildString(char*p) {   
  	char *q=p;
 	int  stringlen=0, i=0,j=1,len=0,maxlen=1;   
