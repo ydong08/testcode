41754aacbef81f73da457aaeada68a79d58d29c7
ydong08 <lfeng1108@163.com>
2019-03-01 17:45:15 +0800

    add util

---
+++ basis/link/dynamic/util.c
@@ -0,0 +1,20 @@
+
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int x, int y) {
+  return x+y;
+}
+
+int div(int a, int b) {
+  return a/b;
+}
+
+int mul(int m, int n) {
+  return m*n;
+}
+
+int sub(int p, int t) {
+  return p-t;
+}
---
+++ basis/link/dynamic/util.h
@@ -0,0 +1,18 @@
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif  
+
+int add(int, int);
+
+int div(int, int);
+
+int mul(int, int);
+
+int sub(int, int);
+
+
+#ifdef __cplusplus
+}
+#endif
