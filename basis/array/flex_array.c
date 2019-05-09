0d4ae6c4134e6edadd946413d0ae8178e0e67f64
ydong08 <lfeng1108@163.com>
2019-05-09 01:07:37 +0000

    move file

--- basis/array/flex_array.c
+++ basis/array/flex_array.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define GROUP_MAX_NUM      10
 #define ADDR_MAX_LEN       10
@@ -19,8 +20,18 @@ typedef struct _GroupInfo
 } GROUPINFO;
 
 int main() {
-	
+	GROUPINFO stGInfo;
+	memset(&stGInfo, 0, sizeof(GROUPINFO));
 	printf("group struct size: %d\n", sizeof(GROUPINFO));
+	printf("group struct var size: %d\n", sizeof(stGInfo));
+	for (int i = 0; i < GROUP_MAX_NUM; ++i) {
+		for (int j = 0; j < ADDR_MAX_LEN; ++j) {
+			stGInfo.num[i] = i;
+			stGInfo.addr[i][j] = i*j;
+		}
+	}
+	
+	printf("group struct var size: %d\n", sizeof(stGInfo));
 	return 0;
 
 }
