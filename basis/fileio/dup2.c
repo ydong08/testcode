04f37987ee6ebae4bd91566dc9270fd47d97bba2
winter <winter@debian.winter>
2020-05-31 21:17:27 -0400

    updat code

--- basis/fileio/dup2.c
+++ basis/fileio/dup2.c
@@ -6,7 +6,7 @@
 
 void main()
 {
-    int oldfd;
+    int oldfd,oldfd2;
     int fd;
     int t;
     char *buf="This is a test!!!!\n";
@@ -16,7 +16,13 @@ void main()
         exit(-1);
     }
 
-    fd=dup2(oldfd,fileno(stdout));
+    if((oldfd2=open("yours.txt",O_RDWR|O_CREAT,0644))==-1)
+    {
+        printf("open error\n");
+        exit(-1);
+    }
+
+    fd=dup2(oldfd,oldfd2);
     if(fd==-1)
     {
         printf("dup2 error\n");
@@ -25,12 +31,20 @@ void main()
     printf("dup2的返回值：%d\n",fd);
     
     t=strlen(buf);
+	usleep(100000000);
+	fprintf(stderr, "begin to close stdout\n");
+	close(fd);
+	fprintf(stderr, "end to close stdout\n");
+	usleep(100000000);
+#if 0
     //本应该写入到stdout的信息，但是标准输出已经重定向到目标文件中，故向标准输出写的数据将会写到目标文件中。
     if(write(fileno(stdout),buf,t)!=t)
     {
         printf("write error!\n");
         exit(-1);
     }
+#endif
     close(fd);
+	close(oldfd);
     exit(0);
 }
