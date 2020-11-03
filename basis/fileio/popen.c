254eb4eb59f36645bc89911e8eeff24c52f8456b
ydong08 <lfeng1108@163.com>
2020-11-03 11:16:49 +0800

    popen code

--- basis/fileio/popen.c
+++ basis/fileio/popen.c
@@ -4,12 +4,29 @@
 
 int main()
 {
+	int i = 0;
+	const char *curl = "/usr/bin/curl www.baidu.com";
 	const char *f = "/home/winter/repo/testcode/basis/fileio/sleep.sh";
 	const char *cmd = "echo 123; sleep 10; echo 456;";
+	char  recv[128] = {0};
 
 	//FILE *fp = popen(cmd, "r");
 	FILE *fp = popen(f, "r");
 	if (fp) pclose(fp);
+
+
+	for (i = 0; i < 100; ++i)
+	{
+		memset(recv, 0, sizeof(recv));
+		fp = popen(curl, "r");
+		if (fp)
+		{
+			fread(recv, sizeof(recv) - 1, 1, fp);
+			printf("recv:%s\n", recv);
+			pclose(fp);
+			usleep(500000);
+		}
+	}
 
 	return 0;
 
