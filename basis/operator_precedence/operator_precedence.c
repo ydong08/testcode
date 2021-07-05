de4d7ec1099dd2f4c017f9d03366a0b6c831f965
ydong08 <lfeng1108@163.com>
2021-07-05 02:34:59 -0400

    add code

--- basis/operator_precedence/operator_precedence.c
+++ basis/operator_precedence/operator_precedence.c
@@ -7,5 +7,18 @@ int main() {
   int multi = num*base++;
   printf("pre-multi: %d\n", multi);
   printf("multi: %d\n", num*base++);
+  /*################表达式计算################*/
+
+  char *ptr = NULL;
+  char name[32] = {"Beijing TianAnMen"};
+  const char *chairman = "Mao Ze Dong";
+
+  if (NULL != (ptr = name, chairman))
+  {
+	printf("str: %s.\n", ptr);
+  }
+  printf("End line.\n");
+
+  return 0;
 
 }
