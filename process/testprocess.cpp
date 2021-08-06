99a2059d89c0016ef6e30689e616bc455a76cfb4
ydong08 <lfeng1108@163.com>
2021-08-06 01:29:50 -0400

    update file

--- process/testprocess.cpp
+++ process/testprocess.cpp
@@ -1,11 +1,8 @@
-<<<<<<< Updated upstream
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 
 #define LOOP 20
 void loop_print(void)
@@ -50,56 +47,3 @@ int main()
  	
  	return 0;
 }
-=======
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#define LOOP 20
-void loop_print(void)
-{
-    int nloop = 0;
-    while(++nloop < LOOP){
-        printf("loop times %d\n", nloop);
-        sleep(1);
-    }
-}
-
-int main()
-{
-    printf("enter fork\n");
-    fflush(NULL);
-	int status;
-    pid_t pid = fork();
-    if (pid < 0 || 0 < pid){
-        perror("fork");
-        exit(1);
-    }
-
-    if (0 == pid )
-	{
-        printf("enter child process\n");
-        //loop_print();
-		fflush(NULL);
-		pid = fork();
-		if (pid < 0)
-		{
-			exit(1);
-		}
-
-		if (pid == 0)
-		{
-			execl("/bin/date", "date", "+%T", NULL);
-        	exit(0);
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-			printf("exit child-parent process\n");		
-		}
-	}
-    printf("will exit parent process\n");
-}
->>>>>>> Stashed changes
